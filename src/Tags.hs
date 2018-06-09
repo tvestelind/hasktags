08da67627e89224287bdac04448f03a2beec3367
Tomas Vestelind <tomas.vestelind@gmail.com>
2018-06-09 23:23:54 +0200

    use optparse-applicative instead of GetOpt
    GetOpt does not really give you that much help when doing argument
    parsing, while optparse on the other hand does a lot of nice things.
    
    This refactor is to make it easier to extend Hasktags with new tags.

--- src/Tags.hs
+++ src/Tags.hs
@@ -148,8 +148,8 @@ dumpThing cond thing = if cond
                           else normalDump thing
 
 -- stuff for dealing with ctags output format
-writectagsfile :: Handle -> Bool -> [FileData] -> IO ()
-writectagsfile ctagsfile extended filedata = do
+writectagsfile :: Bool -> [FileData] -> Handle -> IO ()
+writectagsfile extended filedata ctagsfile = do
     let things = concatMap getfoundthings filedata
     when extended
          (do hPutStrLn
@@ -172,8 +172,8 @@ sortThings = sortBy comp
 
 -- stuff for dealing with etags output format
 
-writeetagsfile :: Handle -> [FileData] -> IO ()
-writeetagsfile etagsfile = mapM_ (hPutStr etagsfile . etagsDumpFileData)
+writeetagsfile :: [FileData] -> Handle -> IO ()
+writeetagsfile fileData etagsfile = mapM_ (hPutStr etagsfile . etagsDumpFileData) fileData
 
 etagsDumpFileData :: FileData -> String
 etagsDumpFileData (FileData filename things) =
